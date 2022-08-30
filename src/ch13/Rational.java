87d6ac59d4c7eae458093f69effc980dd7c4d454
Michael Lu <yeskky@gmail.com>
2022-08-30 17:28:11 +0800

    增加13章的有理数类（未完成）

---
+++ src/ch13/Rational.java
@@ -0,0 +1,55 @@
+package ch13;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Rational extends Number implements Comparable<Rational> {
+    // 分子和分母的数据字段
+    private long numerator = 0;
+    private long denominator = 1;
+
+    public Rational() {
+        this(0, 1);
+    }
+
+    public Rational(long numerator, long denominator) {
+        long gcd = gcd(numerator, denominator);
+        this.numerator = (denominator > 0 ? 1 : -1) * numerator / gcd;
+        this.denominator = Math.abs(denominator) / gcd;
+    }
+
+    private static long gcd(long n, long d) {
+        long n1 = Math.abs(n);
+        long n2 = Math.abs(d);
+        int gcd = 1;
+        for (int k = 1; k <= n1 && k <= n2; k++) {
+            if (n1 % k == 0 && n2 % k == 0)
+                gcd = k;
+        }
+        return gcd;
+    }
+
+    @Override
+    public int compareTo(@NotNull Rational o) {
+        return 0;
+    }
+
+    @Override
+    public int intValue() {
+        return 0;
+    }
+
+    @Override
+    public long longValue() {
+        return 0;
+    }
+
+    @Override
+    public float floatValue() {
+        return 0;
+    }
+
+    @Override
+    public double doubleValue() {
+        return 0;
+    }
+}
--- src/ch19/RationalMatrix.java
+++ src/ch19/RationalMatrix.java
@@ -1,4 +1,18 @@
 package ch19;
-
+import ch13.Rational;
 public class RationalMatrix extends GenericMatrix<Rational>{
+    @Override
+    protected Rational add(Rational o1, Rational o2) {
+        return null;
+    }
+
+    @Override
+    protected Rational multiply(Rational o1, Rational o2) {
+        return null;
+    }
+
+    @Override
+    protected Rational zero() {
+        return null;
+    }
 }
